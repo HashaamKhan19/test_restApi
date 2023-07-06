2087797a093c8a62134b49e490cdd2611b25be35
hashaam <hashaamkhan4247@gmail.com>
2023-07-06 18:43:39 +0500

    created model for product

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 /////////ROUTES/////////
 app.get("/", (req, res) => {
   res.send("Whats up bro");
 });
 
-app.listen(3000, () => {
-  console.log("server started");
-});
+mongoose
+  .connect(
+    "mongodb+srv://hashaam:hk42471072@testcluster.qx7vnml.mongodb.net/NodeApiTest?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("DB Connected");
+    app.listen(3000, () => {
+      console.log("server started");
+    });
+  })
+  .catch((err) => console.log(err));
